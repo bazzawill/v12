76270534cc7181ce43fb6473945d20aed3910caa
Barry Williams <bazzawill@gmail.com>
2026-04-14 07:49:48 +0930

    add icons

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,78 +1,98 @@
 local wk = require("which-key")
 wk.add({
-    {
-      "<leader>-",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
-    },
-    {
-      -- Open in the current working directory
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
-    },
-    { "<leader>r", "<cmd>restart<CR>", desc = "Restart Neovim / Reload Conf", icon = "󰑓 ", mode = "n"},
-    { "<leader>p", group = "Plugins" },
-    { "<leader>pm", "<cmd>Mason<CR>", desc = "Language and Formatters", mode = "n"},
-    { "<leader>f", group = "file" }, -- group
-    {
-        "<leader>ff",
-        "<cmd>FzfLua files<CR>",
-        desc = "Find File",
-        mode = "n",
-    },
-    {
-        "<leader>fk",
-        "<cmd>FzfLua keymap<CR>",
-        desc = "Find Keybinding",
-        mode = "n",
-    },
-    {
-        "<leader>f?",
-        "<cmd>FzfLua<CR>",
-        desc = "Find Menu",
-        mode = "n",
-    },
-    {
-        mode="n", "<leader>fg", "<cmd>FzfLua grep_project<CR>", desc = "Find word in project"
-    },
-    {
-        mode="n", "<leader>fl", "<cmd>FzfLua grep_last<CR>", desc = "Grep Last"
-    },
-    {
-        mode= "n", "<leader>fh", "<cmd>FzfLua help_tags<CR>", desc = "Search Help"
-    },
-    { "<leader>n", "<cmd>enew<CR>", desc = "New File" },
-    { "<leader>t", group = "tabs" },
-    { mode="n", "<Leader>te", "<cmd>tabnew<CR>", desc="new tab" },
-    { mode="n", "<Leader>tn", "<cmd>tabn<CR>", desc="next tab" },
-    { mode="n", "<Leader>tp", "<cmd>tabp<CR>", desc="previous tab" },
-    { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
-    {
-        "<leader>b",
-        group = "buffers",
-        expand = function()
-            return require("which-key.extras").expand.buf()
-        end,
-    },
-    { "<leader>e",
-       function()
-           require("yazi").yazi()
-        end, desc = "File Explorer" },
-    {
-        -- Nested mappings are allowed and can be added in any order
-        -- Most attributes can be inherited or overridden on any level
-        -- There's no limit to the depth of nesting
-        mode = { "n", "v" }, -- NORMAL and VISUAL mode
-        { "<leader>q", "<cmd>confirm q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
-        { "<leader>w", "<cmd>w<cr>", desc = "Write" },
-    },
+	{
+		"<leader>-",
+		mode = { "n", "v" },
+		"<cmd>Yazi<cr>",
+		desc = "Open yazi at the current file",
+		icon = " ",
+	},
+	{
+		-- Open in the current working directory
+		"<leader>cw",
+		"<cmd>Yazi cwd<cr>",
+		desc = "Open the file manager in nvim's working directory",
+		icon = " ",
+	},
+	{
+		"<c-up>",
+		"<cmd>Yazi toggle<cr>",
+		desc = "Resume the last yazi session",
+		icon = " ",
+	},
+	{ "<leader>r", "<cmd>restart<CR>", desc = "Restart Neovim / Reload Conf", icon = "󰑓 ", mode = "n" },
+	{ "<leader>p", group = "Plugins", icon = " " },
+	{ "<leader>pm", "<cmd>Mason<CR>", desc = "Language and Formatters", mode = "n" },
+	{ "<leader>f", group = "find", icon = " " }, -- group
+	{
+		"<leader>ff",
+		"<cmd>FzfLua files<CR>",
+		desc = "Find File",
+		mode = "n",
+		icon = "󰈔 ",
+	},
+	{
+		"<leader>fk",
+		"<cmd>FzfLua keymap<CR>",
+		desc = "Find Keybinding",
+		mode = "n",
+        icon = "󰋖 ",
+	},
+	{
+		"<leader>f?",
+		"<cmd>FzfLua<CR>",
+		desc = "Find Menu",
+		mode = "n",
+	},
+	{
+		mode = "n",
+		"<leader>fg",
+		"<cmd>FzfLua grep_project<CR>",
+		desc = "Find word in project",
+        icon = "󰈞 ",
+	},
+	{
+		mode = "n",
+		"<leader>fl",
+		"<cmd>FzfLua grep_last<CR>",
+		desc = "Grep Last",
+	},
+	{
+		mode = "n",
+		"<leader>fh",
+		"<cmd>FzfLua help_tags<CR>",
+		desc = "Search Help",
+        icon = "󰋖 ",
+	},
+	{ "<leader>n", "<cmd>enew<CR>", desc = "New File" },
+	{ "<leader>t", group = "tabs" },
+	{ mode = "n", "<Leader>te", "<cmd>tabnew<CR>", desc = "new tab" },
+	{ mode = "n", "<Leader>tn", "<cmd>tabn<CR>", desc = "next tab" },
+	{ mode = "n", "<Leader>tp", "<cmd>tabp<CR>", desc = "previous tab" },
+	{ "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+	{
+		"<leader>b",
+		group = "buffers",
+		expand = function()
+			return require("which-key.extras").expand.buf()
+		end,
+	},
+	{
+		"<leader>e",
+		function()
+			require("yazi").yazi()
+		end,
+		desc = "File Explorer",
+	},
+	icon = " ",
+	{
+		-- Nested mappings are allowed and can be added in any order
+		-- Most attributes can be inherited or overridden on any level
+		-- There's no limit to the depth of nesting
+		mode = { "n", "v" }, -- NORMAL and VISUAL mode
+		{ "<leader>q", "<cmd>confirm q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+		{ "<leader>w", "<cmd>w<cr>", desc = "Write" },
+	},
 })
 local keymap = vim.keymap.set
 local s = { silent = true }
@@ -86,8 +106,8 @@ keymap("n", "<C-d>", "<C-d>zz")
 keymap("n", "<C-u>", "<C-u>zz")
 
 --- split windows
-keymap("n", "<Leader>_", "<cmd>vsplit<CR>", { silent = true, desc = "vertical split"})
-keymap("n", "<Leader>-", "<cmd>split<CR>", { silent = true, desc = "horizontal split"})
+keymap("n", "<Leader>_", "<cmd>vsplit<CR>", { silent = true, desc = "vertical split" })
+keymap("n", "<Leader>-", "<cmd>split<CR>", { silent = true, desc = "horizontal split" })
 
 -- copy and paste
 keymap("v", "<Leader>p", '"_dP')
@@ -97,7 +117,7 @@ keymap("x", "y", [["+y]], s)
 keymap("t", "<Esc>", "<C-\\><C-N>")
 
 -- cd current dir
-keymap("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))<CR>', { desc = "cd to current directory"})
+keymap("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))<CR>', { desc = "cd to current directory" })
 
 local ns = { noremap = true, silent = true }
 local er = { expr = true, replace_keycodes = false }
@@ -117,19 +137,19 @@ keymap("n", "<C-n>", require("miniharp").next)
 keymap("n", "<C-p>", require("miniharp").prev)
 -- keymap({ "n", "x" }, "<leader>gy", require("gh-permalink").yank)
 keymap("n", "<leader>so", function()
-    require("fzf-lua").files({
-        actions = {
-            ["default"] = function(selected)
-                local file = selected[1]
-                local rel_path = vim.fn.fnamemodify(file, ":.")
+	require("fzf-lua").files({
+		actions = {
+			["default"] = function(selected)
+				local file = selected[1]
+				local rel_path = vim.fn.fnamemodify(file, ":.")
 
-                rel_path = rel_path:gsub(" ", "\\ ")
-                if not rel_path:match("^%.?/") then
-                    rel_path = "./" .. rel_path
-                end
+				rel_path = rel_path:gsub(" ", "\\ ")
+				if not rel_path:match("^%.?/") then
+					rel_path = "./" .. rel_path
+				end
 
-                vim.api.nvim_put({ rel_path }, "l", true, false)
-            end,
-        },
-    })
+				vim.api.nvim_put({ rel_path }, "l", true, false)
+			end,
+		},
+	})
 end)
